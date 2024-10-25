f80c993661bdc0fe6235016b10ea7cb61806a1c0
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-10-25 03:41:21 +0100

    "Rearranged and reformatted routes in posts.js"

--- routes/posts.js
+++ routes/posts.js
@@ -2,22 +2,26 @@ const express = require("express");
 const router = express.Router();
 const getPosts = require("../controllers/postController");
 
+
 router 
     .route('/')
     .get(getPosts.getFilter)
+    .get(getPosts.getPostLimit)
+    
+router
+  .route("/")
+  .get(getPosts.getAllPost)
 
 router
   .route("/")
-  .get(getPosts.getPostLimit)
-  .get(getPosts.getAllPost)
-  .post(getPosts.createPost);
+  .post(getPosts.createPost)
   
 
 router
   .route("/:id")
   .put(getPosts.updatePost)
   .get(getPosts.getPostID)
-  .delete(getPosts.deletePost);
+  .delete(getPosts.deletePost)
 
-  
+
 module.exports = router;
